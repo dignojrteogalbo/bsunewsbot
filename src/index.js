5bd55f75e7501cf57b8848c81c181435c1c90037
dignojrteogalbo <teogalbo.dj193@hotmail.com>
2021-10-24 14:05:22 -0600

    Anonymous rating

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import md5 from 'md5';
 import Parser from 'rss-parser';
 let parser = new Parser();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
 const RSS_URL = 'https://www.boisestate.edu/news/';
 
 dotenv.config();
@@ -30,6 +30,37 @@ function NewsEmbed({title, creator, info, link, categories, timestamp, imageName
     return { embeds: [embed] };
 }
 
+function RateEmbed(name, rating, content = null) {
+    let embed = new MessageEmbed()
+        .setTitle(name)
+        .setColor(0x0033A0)
+        .setTimestamp(Date.now());
+
+    if (content) {
+        embed.addFields({ name: 'Message: ', value: content, inline: false });
+    }
+
+    switch (rating) {
+        case 1:
+            embed.setDescription('⭐️');
+            break;
+        case 2:
+            embed.setDescription('⭐️⭐️');
+            break;
+        case 3:
+            embed.setDescription('⭐️⭐️⭐️');
+            break;
+        case 4:
+            embed.setDescription('⭐️⭐️⭐️⭐️');
+            break;
+        case 5:
+            embed.setDescription('⭐️⭐️⭐️⭐️⭐️');
+            break;
+    }
+
+    return { embeds: [embed] };
+}
+
 client.once('ready', () => {
     console.log('Ready!');
 });
@@ -38,17 +69,14 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     const { commandName } = interaction;
-    const option =  interaction.options.getString('category') ?? '';
 
     switch (commandName) {
         case 'ping':
             await interaction.reply('Pong!');
-
             break;
         
         case 'server':
             await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-
             break;
 
         case 'user':
@@ -57,6 +85,7 @@ client.on('interactionCreate', async interaction => {
             break;
 
         case 'news':
+            let option = interaction.options.getString('category') ?? '';
             (async () => {
                 await parser.parseURL(RSS_URL + option + '/feed')
                     .then(feed => {
@@ -116,9 +145,44 @@ client.on('interactionCreate', async interaction => {
                                 timestamp: timestamp,
                             }));
                         }
-                    });
+                    }).catch(err => console.log(err));
             })();
             
+            break;
+
+        case 'rate':
+            let name = interaction.options.getString('name');
+            let rating = interaction.options.getInteger('rating');
+            let content = interaction.options.getString('message') ?? '';
+
+            let message = (content != '') ? RateEmbed(name, rating, content) : RateEmbed(name, rating);
+
+            (async() => {
+                await interaction.reply({ content: 'Confirm message in DMs', ephemeral: true });
+                await interaction.user.send(message);
+
+                let confirmMessage = await interaction.user.send('Confirm message (30 secs)');
+                await confirmMessage.react('👍');
+                await confirmMessage.react('👎');
+
+                const filter = (reaction, user) => reaction.emoji.name === '👍' || reaction.emoji.name === '👎' && !user.bot;
+
+                const reactionCollector = confirmMessage.createReactionCollector({ filter, max: 1, time: 30_000 });
+
+                reactionCollector.on('collect', (reaction, user) => {
+                    if (reaction.emoji.name === '👍') {
+                        confirmMessage.edit('Message confirmed.')
+                        interaction.channel.send(message);
+                    } else {
+                        confirmMessage.edit('Message canceled.')
+                    }
+                });
+
+                reactionCollector.on('end', (collected, reason) => {
+                    if (reason === 'time') 
+                        confirmMessage.edit('Confirmation period timed out.');
+                });
+            })();
             break;
     }
 });
