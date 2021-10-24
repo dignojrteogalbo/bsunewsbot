5cea977eb4b4cfc939991ac9167dc1176ad61746
dignojrteogalbo <teogalbo.dj193@hotmail.com>
2021-10-24 14:06:13 -0600

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -6,7 +6,8 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 const clientId = process.env.CLIENTID;
 const token = process.env.TOKEN;
-const guildId = "596320306835095554";
+const guildId = "596320306835095554"; // Test Server
+// const guildId = "882725373459181589"; // BSU CS Students
 
 const newsCommand = new SlashCommandBuilder().setName('news').setDescription('Replies with the latest news story')
     .addStringOption(option =>
@@ -17,12 +18,31 @@ const newsCommand = new SlashCommandBuilder().setName('news').setDescription('Re
             .addChoice('News for the Campus Community', 'category/news-for-the-campus-community')
             .addChoice('Campus Life and Student Success', 'category/campus-life-and-student-success'));
 
+const ratingCommand = new SlashCommandBuilder().setName('rate').setDescription('Send an anonymous rating of an instructor')
+    .addStringOption(option => 
+        option.setName('name')
+            .setDescription('Name of the instructor')
+            .setRequired(true))
+    .addIntegerOption(option =>
+        option.setName('rating')
+            .setDescription('Instructor\'s rating out of 5')
+            .setRequired(true)
+            .addChoice('1 ⭐️', 1)
+            .addChoice('2 ⭐️⭐️', 2)
+            .addChoice('3 ⭐️⭐️⭐️', 3)
+            .addChoice('4 ⭐️⭐️⭐️⭐️', 4)
+            .addChoice('5 ⭐️⭐️⭐️⭐️⭐️', 5))
+    .addStringOption(option =>
+        option.setName('message')
+            .setDescription('Add your message')
+            .setRequired(true));
+
 const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
     new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
     new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-    newsCommand
-    
+    newsCommand,
+    ratingCommand
 ]
     .map(command => command.toJSON());
 
